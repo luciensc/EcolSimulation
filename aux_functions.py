b923ae83d36721aef677dbf2563e562d459747ca
luciensc <56501086+luciensc@users.noreply.github.com>
2021-08-05 06:21:34 +0200

    improve plotting: ylim, save plot during each iteration

--- aux_functions.py
+++ aux_functions.py
@@ -1,22 +1,32 @@
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import numpy as np
+
 # save function
 
+
 # plot functions
-def plot_grid_pop(grid, iter, log, n_run, block_img=False, save=False):
+def plot_grid_pop(grid, t, log, n_run, save_path=None, ylim=None, block_img=False, save=False):
     """
     plot the spatial representation (left subplot) and population over time (right subplot)
     """
-    plt.close()
     fig, (ax1, ax2) = plt.subplots(1, 2)
-    fig.suptitle(f"iteration = {iter}")
+    fig.suptitle(f"iteration = {t}")
     ax1.imshow(grid.reproduction, cmap=cm.get_cmap("Greens"), vmin=0, )
     ax1.set_title(f"spatial distribution of reproductive potential")
     ax2.plot(np.array(log))
     ax2.scatter(range(len(log)), np.array(log))
     ax2.set_title("population dynamics")
     ax2.set_xlim([0, n_run])
+    if ylim is None:
+        ax2.set_ylim([0, max(log)])
+    else:
+        ax2.set_ylim([0, ylim])
     plt.show(block=block_img)
     plt.pause(0.00001)
-    # TODO: implement saving function. requires path.
+
+    if save_path:
+        plt.savefig(save_path + "plot_"+str(t))
+
+    plt.close()
+
--- main.py
+++ main.py
@@ -17,11 +17,9 @@ ecological simulation
 
 seasonal organism
 """
-save = True
-description = "uniform_decay4"  # optional descriptor of current experiment for logging purposes
 
-n_run = 30
-plot_freq = 5
+n_run = 5
+plot_freq = 1
 
 LENGTH = 50  # square grid length
 DISPERSAL_DECAY = 4  # exponent for decay. higher exponent -> faster decay.
@@ -30,26 +28,47 @@ ecol_distr_types = ["random_uniform", "random_binary"]
 ECOL_DISTR = ecol_distr_types[0]
 BERNOULLI = False
 
-seed_sim = 42
-seed_ecol = 1859
-seed_spawn = 376
+seed_ecol = 1859  # initialisation of fitness distribution (grid.ecol)
+seed_spawn = 376  # selection of intially populated cells in the grid
+seed_sim = 42  # stochastic effects during simulation
 
-params = {"n_run": n_run, "length":LENGTH, "dispersal_decay":DISPERSAL_DECAY, "n_spawn":N_SPAWN,
-          "ecol_distr":ECOL_DISTR, "seed_sim":seed_sim, "seed_ecol":seed_ecol, "seed_spawn":seed_spawn,
-          "bernoulli":BERNOULLI}
+save = True
+gif = True  # may only be True if 'save' is True
+# optional descriptor of current experiment for logging purposes
+description = "VARIANCE__decay" + str(DISPERSAL_DECAY) + "_ecol" + str(seed_ecol) + "_spawn" + str(seed_spawn)
 
 #####################################
+
+# TODO: check convention for conditionally required/defined variables
+path = None
+gif_path = None
+if save:
+    path = "experiments/"
+    path += dtt.now().strftime("%d%m%Y_%H%M")
+    if description != "":
+        path += "_" + description
+    path += "/"
+    os.mkdir(path)
+
+    # save each iteration's plot
+    if gif:
+        gif_path = path + "gif/"
+        os.mkdir(gif_path)
+
 
 grid = Grid(length=LENGTH, disp_decay=DISPERSAL_DECAY, ecol_distr=ECOL_DISTR, n_spawn=N_SPAWN, seed_ecol=seed_ecol,
             seed_spawn=seed_spawn)
 
 matplotlib.rcParams['figure.figsize'] = [14.0, 6.0]
 
+# plot initial resource distribution
 plt.imshow(grid.ecol, cmap=cm.get_cmap("Blues"), vmin=0, vmax=1,)
-plt.title(f"resource distribution. total resources: {np.rint(np.sum(np.sum(grid.ecol)))}")
+max_resources = np.sum(np.sum(grid.ecol))
+plt.title(f"resource distribution. total resources: {int(np.rint(max_resources))}")
 plt.show()
 
 log = []
+
 ### START SIMULATION
 t0 = time.time()
 np.random.seed(seed_sim)
@@ -61,33 +80,30 @@ for t in range(n_run):
 
     # PLOT REPRODUCTIVE POTENTIAL
     if t%plot_freq == 0:
-        plot_grid_pop(grid, t, log, n_run, block_img=False)
+        plot_grid_pop(grid, t, log, n_run, save_path=gif_path, ylim=max_resources, block_img=False)
 
 delta_t = time.time() - t0
-print(f"duration of loop: {np.rint(delta_t)} s\naverage iteration duration: {np.round(delta_t/n_run, 2)} s")
+print(f"duration of loop: {np.rint(delta_t)} s\naverage time per iteration: {np.round(delta_t/n_run, 2)} s")
 
 # save log, parameters, and population dynamics plot into folder
 if save:
-    path = "experiments/"
-    path += dtt.now().strftime("%d%m%Y_%H%M")
-    if description != "":
-        path += "_" + description
-    path += "/"
-    os.mkdir(path)
-
-    param_file = open(path+"params_dict.pck", 'wb')
+    params = {"n_run": n_run, "length": LENGTH, "dispersal_decay": DISPERSAL_DECAY, "n_spawn": N_SPAWN,
+              "ecol_distr": ECOL_DISTR, "seed_sim": seed_sim, "seed_ecol": seed_ecol, "seed_spawn": seed_spawn,
+              "bernoulli": BERNOULLI}
+    param_file = open(path + "params_dict.pck", 'wb')
     pickle.dump(params, param_file)
     param_file.close()
 
-    log_file = open(path+"log.pck", 'wb')
+    log_file = open(path + "log.pck", 'wb')
     pickle.dump(log, log_file)
     log_file.close()
 
+    plt.close()
     plt.plot(np.array(log), color="black")
     plt.scatter(range(len(log)), np.array(log), color="black")
     plt.title("population dynamics")
     plt.xlim([0, n_run])
-    # TODO: ylim based on expected maximum
-    plt.savefig(path+"pop_dyn")
+    plt.ylim([0, max_resources])
+    plt.savefig(path + "pop_dyn")
 
 
